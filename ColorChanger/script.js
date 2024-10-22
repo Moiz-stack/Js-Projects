8e30035a2dcb7568264764a5e6d777c40b914b14
Moiz-stack <moiz.workmail@gmail.com>
2024-10-22 12:41:01 -0700

    clock project added

--- ColorChanger/script.js
+++ ColorChanger/script.js
@@ -1,28 +1,23 @@
-let button = document.querySelectorAll(".button")
-let body = document.querySelector("body")
+let button = document.querySelectorAll(".button");
 
-button.forEach(btn => {
+let body=document.querySelector("body");
 
-    btn.addEventListener("click", function (e) {
-        console.log(e.target.id)
-        if (e.target.id === "grey") {
-            body.style.backgroundColor = e.target.id
-        }
-        else if(e.target.id === "white"){
-            body.style.backgroundColor = e.target.id
-        }
-        else if(e.target.id === "blue"){
-            body.style.backgroundColor = e.target.id
-        }
-        else if(e.target.id === "yellow"){
-            body.style.backgroundColor = e.target.id
-        }
-        else{
-            alert("Invalid Color")
-        }
+// console.log(button)
 
+    
+button.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        if(e.target.id == 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id == 'white'){
+            body.style.backgroundColor = e.target.id;   
+        }
+        else if(e.target.id == 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+        else if(e.target.id == 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
     })
-
-});
-
-console.log(button)
+})
